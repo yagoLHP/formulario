073cc0a5966653956014905a5f398f0aa4a8dadb
yagoLHP <yaguimzl@outlook.com>
2025-11-03 09:21:54 -0300

    Alteração

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ server.get("/", (req, res) => {
             <head>
             <meta charset="UTF-8"/> 
             <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"/>
-            <title>PPI 2</title>
+            <title>CBF</title>
             </head>
             <body>
             <nav class="navbar navbar-expand-lg bg-body-tertiary">
